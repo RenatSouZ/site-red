8c0579cdb5960d70e2dbee30e0bcb8ca316c34d2
RenatSouz <rcsouza1987@gmail.com>
2025-04-30 22:06:16 -0300

    Atualização com favicon e formulário WhatsApp

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function toggleMenu() {
   menu.classList.toggle('show');
   toggleButton.classList.toggle('active');
 }
+function enviarFormulario(event) {
+  event.preventDefault();
+
+  const nome = document.getElementById('nome').value;
+  const mensagem = document.getElementById('mensagem').value;
+
+  const texto = `Olá, meu nome é ${nome}. Gostaria de falar sobre: ${mensagem}`;
+  const numeroWhatsApp = '5521992888989'; // Coloque o número com DDD e sem espaços
+
+  const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(texto)}`;
+  window.open(url, '_blank');
+}
